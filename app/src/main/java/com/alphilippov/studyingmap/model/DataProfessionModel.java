35bec20bcbdb258ad385a6dde80c8563a8c49030
AlFilippov <41473157+alfilippov@users.noreply.github.com>
2019-09-02 11:50:34 +0300

    -create model layer -add Room lib

--- app/src/main/java/com/alphilippov/studyingmap/model/DataProfessionModel.java
+++ app/src/main/java/com/alphilippov/studyingmap/model/DataProfessionModel.java
@@ -1,12 +1,22 @@
 package com.alphilippov.studyingmap.model;
 
+import android.content.Context;
+import android.util.Log;
+
 import com.alphilippov.studyingmap.database.AppDataBase;
 import com.alphilippov.studyingmap.database.dao.ProfessionDao;
 import com.alphilippov.studyingmap.database.entity.ProfessionEntity;
 import com.alphilippov.studyingmap.repository.Repo;
 import com.alphilippov.studyingmap.utils.AppApplication;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
+
+import static android.support.constraint.Constraints.TAG;
 
 public class DataProfessionModel implements Repo {
 
@@ -35,4 +45,30 @@ public class DataProfessionModel implements Repo {
         ProfessionDao professionDao = db.partOneProfessionDao();
         List<ProfessionEntity> professionEntities = professionDao.getAll();
     }
+//TODO:Скопировать 3 файла в директорию room при первой инициализации
+    private void copyAttachedDatabase(Context context, String databaseName) {
+        final File dbPath = context.getDatabasePath(databaseName);
+        if (dbPath.exists()) {
+            return;
+        }
+        try {
+            final InputStream inputStream = context.getAssets().open(databaseName);
+            final OutputStream output = new FileOutputStream(dbPath);
+            byte[] buffer = new byte[8192];
+            int length;
+
+            while ((length = inputStream.read(buffer, 0, 8192)) > 0) {
+                output.write(buffer, 0, length);
+            }
+
+            output.flush();
+            output.close();
+            inputStream.close();
+        } catch (IOException e) {
+            Log.d(TAG, "Failed to open file", e);
+            e.printStackTrace();
+        }
+    }
 }
+
+
